249529495762f19e09b98320f0bfed5df7cc05db
AfootDesert41 <AfootDesert41@users.noreply.github.com>
2019-09-21 21:02:36 -0400

    Uploading files

---
+++ precache-manifest.ed992849c41d906b5b5b529c58591543.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2721a3b5ac875cb62a973cb9bc19dfec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5dbc2c8b7b7a6747561",
+    "url": "/static/css/2.ea35eab1.chunk.css"
+  },
+  {
+    "revision": "2a07eca03d730c9895a3",
+    "url": "/static/css/main.a678e121.chunk.css"
+  },
+  {
+    "revision": "d5dbc2c8b7b7a6747561",
+    "url": "/static/js/2.ba60d29d.chunk.js"
+  },
+  {
+    "revision": "2a07eca03d730c9895a3",
+    "url": "/static/js/main.3ebb0494.chunk.js"
+  },
+  {
+    "revision": "0672c2c3c1d0badaae8a",
+    "url": "/static/js/runtime-main.ff01c3a1.js"
+  },
+  {
+    "revision": "17fe290341c062f193a9682f836015d3",
+    "url": "/static/media/Codec-Warm-ExtraBold-trial.17fe2903.ttf"
+  },
+  {
+    "revision": "a7e8347ac0bf5ff6aa813c0e8b5c23bc",
+    "url": "/static/media/Codec-Warm-ExtraLight-trial.a7e8347a.ttf"
+  },
+  {
+    "revision": "73bbeccd5650462d947342aa9139540b",
+    "url": "/static/media/Codec-Warm-Regular-trial.73bbeccd.ttf"
+  },
+  {
+    "revision": "6dca0e3de1f010f5b32d6ccd423265ac",
+    "url": "/static/media/gear.6dca0e3d.png"
+  },
+  {
+    "revision": "03b98b2c5c4b79f919b03cded2fc93f6",
+    "url": "/static/media/pic.03b98b2c.jpg"
+  }
+]);
